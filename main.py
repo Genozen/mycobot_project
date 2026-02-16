fde6232cb4c089a46fa85f514939bd62bcb8c1a6
Genozen <nathanbiomedeng@gmail.com>
2026-02-16 12:36:02 +0800

    rubber ducky grabs!

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from ducky_detector import DuckyDetector
 import time
 import cv2
 
+
+DUCK_CENTER = (230, 230) # x, y of the center coordinate form the camera reading
+DISTANCE_THRESHOLD = 5
+INCREMENT = 2
+PIXEL_TO_MM_RATIO = 0.497 # mm / px
+
 def test_mycobot():
     mycobot = MyCobotManager()
     mycobot.gripper_open()
@@ -21,12 +27,23 @@ def test_mycobot():
     mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
     time.sleep(0.3)
 
+    while True:
+        new_pose = mycobot.current_pose.copy()
+        new_pose[0] += INCREMENT
+        print("move right: ", new_pose)
+        mycobot.move_to_pose(new_pose)
+        print("current pose: ", mycobot.current_pose)
+        time.sleep(0.3)
+
 def test_ducky_detector():
     ducky_detector = DuckyDetector()
     ducky_detector.run()
 
 def main():
     mycobot = MyCobotManager()
+    mycobot.gripper_open()
+    mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
+    
     ducky_detector = DuckyDetector()
     ducky_detector.start()  # Non-blocking start in separate thread
 
@@ -35,11 +52,71 @@ def main():
             # Display frames (must be called from main thread for OpenCV GUI)
             ducky_detector.display_frames()
             
+            mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
+            print("Scanning for ducky...")
+            time.sleep(3.0)
+
             # Get ducky position
             ducky_x, ducky_y = ducky_detector.get_ducky_position()
             if ducky_x is not None and ducky_y is not None:
                 print(f"Ducky at: ({ducky_x:.2f}, {ducky_y:.2f})")
-            
+
+
+                pix_dist_x = ducky_x - DUCK_CENTER[0]
+                robot_dist_x = pix_dist_x * PIXEL_TO_MM_RATIO
+                print("pix dist:", pix_dist_x)
+                print("robot dist:", robot_dist_x)
+                new_pose = mycobot.current_pose.copy()
+                new_pose[0] += robot_dist_x
+                # new_pose[2] = 114.0
+                # print("move to: ", new_pose)
+                # mycobot.move_to_pose(new_pose)
+                # print("current pose: ", mycobot.current_pose)
+
+                pix_dist_y = ducky_y - DUCK_CENTER[1]
+                robot_dist_y = pix_dist_y * PIXEL_TO_MM_RATIO
+                print("pix dist:", pix_dist_y)
+                print("robot dist:", robot_dist_y)
+                # new_pose = mycobot.current_pose.copy()
+                new_pose[1] -= robot_dist_y
+                new_pose[2] = 118.0
+                print("move to: ", new_pose)
+                mycobot.move_to_pose(new_pose)
+
+
+                # if ducky is to the right of the center, move arm to the right
+                # if ducky_x > DUCK_CENTER[0] + DISTANCE_THRESHOLD:
+                #     print("pix dist:", ducky_x - DUCK_CENTER[0])
+                #     new_pose = mycobot.current_pose.copy()
+                #     new_pose[0] += INCREMENT
+                #     print("move right: ", new_pose)
+                #     # mycobot.move_to_pose(new_pose)
+                #     # print("current pose: ", mycobot.current_pose)
+                #     # temp_total_increment += INCREMENT
+                # elif ducky_x < DUCK_CENTER[0] - DISTANCE_THRESHOLD:
+                #     new_pose = mycobot.current_pose.copy()
+                #     new_pose[0] -= INCREMENT
+                #     print("move left: ", new_pose)
+                #     # mycobot.move_to_pose(new_pose)
+                #     # print("current pose: ", mycobot.current_pose)
+
+
+                # if ducky is to the right of the center, move arm to the right
+                # if ducky_y > DUCK_CENTER[1] + DISTANCE_THRESHOLD:
+                #     new_pose = mycobot.current_pose.copy()
+                #     new_pose[1] -= INCREMENT
+                #     print("move down: ", new_pose)
+                #     mycobot.move_to_pose(new_pose)
+                #     print("current pose: ", mycobot.current_pose)
+                # elif ducky_y < DUCK_CENTER[1] - DISTANCE_THRESHOLD:
+                #     new_pose = mycobot.current_pose.copy()
+                #     new_pose[1] += INCREMENT
+                #     print("move up: ", new_pose)
+                #     mycobot.move_to_pose(new_pose)
+                #     print("current pose: ", mycobot.current_pose)
+
+
+
             # cv2.waitKey MUST be called from main thread for GUI to update
             if cv2.waitKey(10) & 0xFF == ord("q"):
                 break
@@ -50,7 +127,25 @@ def main():
         ducky_detector.stop()
 
 if __name__ == "__main__":
+    main()
     # test_mycobot()
     # test_camera()
     # test_ducky_detector()
-    main()
+
+
+"""
+# ducky pixel (x,y)
+ducky_pixel_1 = (348, 247.13)
+ducky_pixel_2 = (460, 246)
+
+# ducky robot pose (x, y)
+ducky_robot_pose_1 = (176, -54.8)
+ducky_robot_pose_2 = (231.7, -42.6)
+
+# calculate the delta between the two points
+delta_distance_px = sqrt((348 - 460)^2 + (247.13 - 246)^2) = 112.0
+delta_distance_robot = sqrt((176 - 231.7)^2 + (-54.8 - (-42.6))^2) = 55.7
+
+pixel distance to robot distance ratio = 112.0 / 55.7 = 2.01
+55.7 / 112.0 = 0.497
+"""
--- mycobot_manager.py
+++ mycobot_manager.py
@@ -12,8 +12,11 @@ class MyCobotManager:
     def __init__(self):
 
         # Pre-set poses
-        self.DUCK_DETECT_POSE = [170.5, -58.3, 182.7, 180, -5, -45]
+        # self.DUCK_DETECT_POSE = [170.5, -58.3, 190.7, 180, -5, -45]
+        self.DUCK_DETECT_POSE = [115.5, -56.2, 284.6, -178.73, -4.05, -39.3]
         self.HUMAN_DETECT_POSE = [86.6, -121.2, 389.9, -80.09, 42.7, -105.22]
+
+        self.current_pose = None
 
         # Initialize MyCobot280
         self.mc = MyCobot280(PI_PORT, PI_BAUD)
@@ -24,6 +27,9 @@ class MyCobotManager:
         if self.mc.get_fresh_mode() != 1:
             self.mc.set_fresh_mode(1)
 
+        # get current pose
+        self.current_pose = self.mc.get_coords()
+
     def gripper_open(self):
         self.mc.set_gripper_state(0, 80)
 
@@ -32,3 +38,23 @@ class MyCobotManager:
 
     def move_to_pose(self, pose):
         self.mc.send_coords(pose, self.SPEED, self.MODE)
+        
+        # naive way of update x, y without verifying...
+        # self.current_pose[0] = pose[0]
+        # self.current_pose[1] = pose[1]
+
+        self.set_current_pose()
+
+    def set_current_pose(self):
+        curr_pose = self.mc.get_coords()
+        # only update x, y
+        self.current_pose[0] = curr_pose[0]
+        self.current_pose[1] = curr_pose[1]
+        # maintain same camera orientation and z
+        self.current_pose[2] = self.DUCK_DETECT_POSE[2]
+        self.current_pose[3] = self.DUCK_DETECT_POSE[3]
+        self.current_pose[4] = self.DUCK_DETECT_POSE[4]
+        self.current_pose[5] = self.DUCK_DETECT_POSE[5]
+
+    def get_current_pose(self):
+        return self.mc.get_coords()
