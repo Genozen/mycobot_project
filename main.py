1b5640e28e7018eb6d5dfb1e2e5c9ee81881098d
Genozen <nathanbiomedeng@gmail.com>
2026-02-18 13:02:50 +0800

    working ducky and face, need to fix oscillation

--- main.py
+++ main.py
@@ -44,100 +44,106 @@ def main():
     camera = Camera(width=640, height=480)
     
     ducky_detector = DuckyDetector()
-    # ducky_detector.start()  # Non-blocking start in separate thread
     face_detector = FaceDetector()
 
-    mycobot.move_to_pose(mycobot.HUMAN_DETECT_POSE)
+    mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
 
-    display_counter = 0 
+    user_mode = "DUCK" # "DUCK" or "HUMAN"
+    
     try:
         while camera.is_opened():
 
             # Display frames (must be called from main thread for OpenCV GUI)
             # ducky_detector.display_frames()
             
-            # mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
-            # time.sleep(3.0) # wait for robot movement to settle
+            if user_mode == "DUCK":
+                mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
+                time.sleep(3.0) # wait for robot movement to settle
             
-            print("Scanning for ducky...")
-            ret, frame = camera.read()
-            if not ret:
-                break
+                print("Scanning for ducky...")
+                ret, frame = camera.read()
+                if not ret:
+                    break
 
-            # ducky_detector.detect(frame)
+                ducky_detector.detect(frame)
 
-            # # Get ducky position
-            # ducky_x, ducky_y = ducky_detector.get_ducky_position()
-            # if ducky_x is not None and ducky_y is not None:
-            #     print(f"Ducky at: ({ducky_x:.2f}, {ducky_y:.2f})")
+                # Get ducky position
+                ducky_x, ducky_y = ducky_detector.get_ducky_position()
+                if ducky_x is not None and ducky_y is not None:
+                    print(f"Ducky at: ({ducky_x:.2f}, {ducky_y:.2f})")
 
-            #     pix_dist_x = ducky_x - DUCK_CENTER[0]
-            #     robot_dist_x = pix_dist_x * PIXEL_TO_MM_RATIO
-            #     print("pix dist:", pix_dist_x)
-            #     print("robot dist:", robot_dist_x)
-            #     new_pose = mycobot.current_pose.copy()
-            #     new_pose[0] += robot_dist_x
+                    pix_dist_x = ducky_x - DUCK_CENTER[0]
+                    robot_dist_x = pix_dist_x * PIXEL_TO_MM_RATIO
+                    print("pix dist:", pix_dist_x)
+                    print("robot dist:", robot_dist_x)
+                    new_pose = mycobot.current_pose.copy()
+                    new_pose[0] += robot_dist_x
 
-            #     pix_dist_y = ducky_y - DUCK_CENTER[1]
-            #     robot_dist_y = pix_dist_y * PIXEL_TO_MM_RATIO
-            #     print("pix dist:", pix_dist_y)
-            #     print("robot dist:", robot_dist_y)
-            #     new_pose[1] -= robot_dist_y
-            #     new_pose[2] = 140.0
-            #     print("move to: ", new_pose)
-            #     mycobot.move_to_pose(new_pose)
-            #     time.sleep(0.5)
-            #     mycobot.gripper_close()
+                    pix_dist_y = ducky_y - DUCK_CENTER[1]
+                    robot_dist_y = pix_dist_y * PIXEL_TO_MM_RATIO
+                    print("pix dist:", pix_dist_y)
+                    print("robot dist:", robot_dist_y)
+                    new_pose[1] -= robot_dist_y
+                    new_pose[2] = 135.0
+                    print("move to: ", new_pose)
+                    mycobot.move_to_pose(new_pose)
+                    time.sleep(0.5)
+                    mycobot.gripper_close()
 
-            #     # move upwards to prevent diagonal movement which drops the ducky
-            #     above_ducky_pose = new_pose.copy()
-            #     above_ducky_pose[2] += 50
-            #     mycobot.move_to_pose(above_ducky_pose)
-            #     time.sleep(1.0)
+                    # move upwards to prevent diagonal movement which drops the ducky
+                    above_ducky_pose = new_pose.copy()
+                    above_ducky_pose[2] += 50
+                    mycobot.move_to_pose(above_ducky_pose)
+                    time.sleep(1.0)
 
-            #     mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
-            #     mycobot.move_to_pose(above_ducky_pose)
-            #     mycobot.move_to_pose(new_pose)
-            #     mycobot.gripper_open()
+                    mycobot.move_to_pose(mycobot.DUCK_DETECT_POSE)
+                    # mycobot.move_to_pose(above_ducky_pose)
+                    # mycobot.move_to_pose(new_pose)
+                    # mycobot.gripper_open()
+
+                    user_mode = "HUMAN"
+                    mycobot.move_to_pose(mycobot.HUMAN_DETECT_POSE)
+
+            elif user_mode == "HUMAN":
 
             ## FACE DETECTION MODE
             # mycobot.move_to_pose(mycobot.HUMAN_DETECT_POSE)
             # time.sleep(3.0) # wait for robot movement to settle
             
 
-            print("Scanning for human...")
-            ret, frame = camera.read()
-            if not ret:
-                break
+                print("Scanning for human...")
+                ret, frame = camera.read()
+                if not ret:
+                    break
 
-            cv2.imshow("frame", frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+                cv2.imshow("frame", frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
 
-            face_detector.detect(frame)
-            face_x, face_y = face_detector.get_face_position()
-            # if face_x is not None and face_y is not None:
-            #     print(f"Human at: ({face_x:.2f}, {face_y:.2f})")
+                face_detector.detect(frame)
+                face_x, face_y = face_detector.get_face_position()
+                # if face_x is not None and face_y is not None:
+                #     print(f"Human at: ({face_x:.2f}, {face_y:.2f})")
 
-            if face_x is not None and face_y is not None:
-                print(f"Human at: ({face_x}, {face_y})")
+                if face_x is not None and face_y is not None:
+                    print(f"Human at: ({face_x}, {face_y})")
 
-                dist_x = abs(face_x - (FACE_CENTER[0] + CENTER_SHIFT + CENTER_THRESHOLD))
-                if face_x > FACE_CENTER[0] + CENTER_SHIFT + CENTER_THRESHOLD:
-                    mycobot.mc.jog_increment_angle(joint_id=1, increment=-dist_x / 7, speed=100, _async=True)
-                    print("moving right")
-                elif face_x < FACE_CENTER[0] + CENTER_SHIFT - CENTER_THRESHOLD:
-                    mycobot.mc.jog_increment_angle(joint_id=1, increment=dist_x / 7, speed=100, _async=True)
-                    print("moving left")
+                    dist_x = abs(face_x - (FACE_CENTER[0] + CENTER_SHIFT + CENTER_THRESHOLD))
+                    if face_x > FACE_CENTER[0] + CENTER_SHIFT + CENTER_THRESHOLD:
+                        mycobot.mc.jog_increment_angle(joint_id=1, increment=-dist_x / 7, speed=100, _async=True)
+                        print("moving right")
+                    elif face_x < FACE_CENTER[0] + CENTER_SHIFT - CENTER_THRESHOLD:
+                        mycobot.mc.jog_increment_angle(joint_id=1, increment=dist_x / 7, speed=100, _async=True)
+                        print("moving left")
 
-                dist_y = abs(face_y - (FACE_CENTER[1] + CENTER_THRESHOLD))
-                if face_y > FACE_CENTER[1] + CENTER_THRESHOLD:
-                    mycobot.mc.jog_increment_angle(joint_id=4, increment=-dist_y / 7, speed=100, _async=True)
-                    print("moving up")
-                elif face_y < FACE_CENTER[1] - CENTER_THRESHOLD:
-                    mycobot.mc.jog_increment_angle(joint_id=4, increment=dist_y / 7, speed=100, _async=True)
-                    print("moving down")
-                time.sleep(0.1)
+                    dist_y = abs(face_y - (FACE_CENTER[1] + CENTER_THRESHOLD))
+                    if face_y > FACE_CENTER[1] + CENTER_THRESHOLD:
+                        mycobot.mc.jog_increment_angle(joint_id=4, increment=-dist_y / 7, speed=100, _async=True)
+                        print("moving up")
+                    elif face_y < FACE_CENTER[1] - CENTER_THRESHOLD:
+                        mycobot.mc.jog_increment_angle(joint_id=4, increment=dist_y / 7, speed=100, _async=True)
+                        print("moving down")
+                    time.sleep(0.1)
 
 
 
